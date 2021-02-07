6e86adf1097f2ba8d103095a74083c60dca84686
A3Tom <Tom_Esq@Yahoo.com>
2021-02-07 21:26:54 +0000

    Added base ass ng project + sample environment file

---
+++ src/environments/environment.sample.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "xxxxx",
+    authDomain: "xxxxx.firebaseapp.com",
+    projectId: "xxxxx",
+    storageBucket: "xxxxx.appspot.com",
+    messagingSenderId: "xxxxx",
+    appId: "xxxxx",
+    measurementId: "xxxxx"
+    }
+};
